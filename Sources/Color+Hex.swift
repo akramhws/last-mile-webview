614bfd78d274785278c6759cad006dc74afff63f
akramhws <akramalhwsawy409@gmail.com>
2026-05-23 01:00:02 +0300

    add file

---
+++ Sources/Color+Hex.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        var h = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        if h.hasPrefix("#") { h = String(h.dropFirst()) }
+        var rgb: UInt64 = 0
+        Scanner(string: h).scanHexInt64(&rgb)
+        let r = Double((rgb >> 16) & 0xFF) / 255
+        let g = Double((rgb >>  8) & 0xFF) / 255
+        let b = Double( rgb        & 0xFF) / 255
+        self.init(red: r, green: g, blue: b)
+    }
+}
